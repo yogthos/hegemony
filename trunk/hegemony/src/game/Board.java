71bdad540d5b8281819d106d697c311a5c05d54d
dmitri.sotnikov <dmitri.sotnikov@97c0824a-6a7c-11de-873c-fb8c264d332a>
2009-07-09 23:22:28 +0000

    added edge overlays when placing edges

--- trunk/hegemony/src/game/Board.java
+++ trunk/hegemony/src/game/Board.java
@@ -51,7 +51,7 @@ public class Board {
 										
 		Edge leftEdge = v.getLeftEdge();
 		if (null != leftEdge) {			
-			if (leftEdge.isActive()) {
+			if (leftEdge.isActive() || leftEdge.isSelected()) {
 				g.drawImage(leftEdge.draw(),leftEdge.getPosX()  + Vertex.SIZE, leftEdge.getPosY(), null);
 			}
 			draw(leftEdge.getSecond(), g);
@@ -63,7 +63,7 @@ public class Board {
 		
 		Edge bottomEdge = v.getBottomEdge();
 		if (null != bottomEdge) {
-			if (bottomEdge.isActive()) {
+			if (bottomEdge.isActive() || bottomEdge.isSelected()) {
 				g.drawImage(bottomEdge.draw(),bottomEdge.getPosX() + Vertex.SIZE, bottomEdge.getPosY() + Vertex.SIZE, null);
 			}
 			draw(bottomEdge.getSecond(), g);
@@ -106,18 +106,18 @@ public class Board {
 	public void handleAction(int x, int y) {
 		double xPos = x/(double)Edge.LENGTH;
 		double yPos = y/(double)Edge.LENGTH;
-		System.out.println(xPos + "," + yPos);
-		findEdge(xPos,yPos);
+		
+		updateEdgeStatus(xPos,yPos);
 	}
 	
-	private void findEdge(double x, double y){
+	private void updateEdgeStatus(double x, double y){
 		int xi = (int)x;
 		int yi = (int)y;
 
 		Vertex v = vertices[xi][yi];
 		double xOffset = x - xi;
 		double yOffset = y - yi;
-		System.out.println(xOffset + ","+yOffset);
+		
 		if (Math.abs(0.5 - xOffset) > Math.abs(0.5 - yOffset)) {
 			if (0.5 > xOffset) {
 				v.getBottomEdge().setActive(!v.getBottomEdge().isActive());				
@@ -134,5 +134,67 @@ public class Board {
 				v.getLeftEdge().setActive(!v.getLeftEdge().isActive());
 			}
 		}	
+	}
+	
+	private int oldXi;
+	private int oldYi;
+	
+	private void deselectEdges(Vertex v) {
+		if (null == v)
+			return;
+		
+		Edge leftEdge = v.getLeftEdge();
+		if (null != leftEdge) {
+			v.getLeftEdge().setSelected(false);
+			deselectEdges(v.getLeftEdge().getSecond());
+		}
+		Edge bottomEdge = v.getBottomEdge();
+		if (null != bottomEdge) {
+			bottomEdge.setSelected(false);
+			deselectEdges(v.getBottomEdge().getSecond());
+		}
+	}
+		
+	private void selectEdge(double x, double y){
+		int xi = (int)x;
+		int yi = (int)y;
+
+		if (xi == oldXi && yi == oldYi) 
+			return;
+		
+		oldXi = xi;
+		oldYi = yi;
+		
+		deselectEdges(vertices[0][0]);
+		
+		Vertex v = vertices[xi][yi];
+		double xOffset = x - xi;
+		double yOffset = y - yi;
+
+		if (Math.abs(0.5 - xOffset) > Math.abs(0.5 - yOffset)) {
+			if (0.5 > xOffset) {
+				v.getBottomEdge().setSelected(!v.getBottomEdge().isSelected());				
+			} else {				
+				Vertex v1 = v.getLeftEdge().getSecond(); 
+				v1.getBottomEdge().setSelected(!v1.getBottomEdge().isSelected());
+			}
+		}
+		else {
+			if (yOffset > 0.5) {			
+				Vertex v1 = v.getBottomEdge().getSecond();
+				v1.getLeftEdge().setSelected(!v1.getLeftEdge().isSelected());
+			} else {
+				v.getLeftEdge().setSelected(!v.getLeftEdge().isSelected());
+			}
+		}	
+	}
+
+
+	public void createOverlay(int x, int y) {
+		
+		double xPos = x/(double)Edge.LENGTH;
+		double yPos = y/(double)Edge.LENGTH;
+				
+		selectEdge(xPos,yPos);		
 	}
 } 
