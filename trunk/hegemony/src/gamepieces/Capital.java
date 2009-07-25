0001b347c01e3177fdfdc8a5db6b76b218d299ba
dmitri.sotnikov <dmitri.sotnikov@97c0824a-6a7c-11de-873c-fb8c264d332a>
2009-07-25 06:32:17 +0000

    added sprites

--- trunk/hegemony/src/gamepieces/Capital.java
+++ trunk/hegemony/src/gamepieces/Capital.java
@@ -2,8 +2,12 @@ package gamepieces;
 
 public class Capital extends GamePiece {
 
-private int value = 1;
+	private int value = 1;
 	
+	public Capital () {
+		sprites = new String[]{"snow/temple0","snow/temple1","snow/temple2","snow/temple3","snow/temple2","snow/temple1",};
+	}
+
 	public int getValue() {
 		return value;
 	}
