702c1b0704a59fc72727d2241e521c87c80b665e
dmitri.sotnikov <dmitri.sotnikov@97c0824a-6a7c-11de-873c-fb8c264d332a>
2009-07-27 03:50:30 +0000

    added beacon sprite for village

--- trunk/hegemony/src/gamepieces/Village.java
+++ trunk/hegemony/src/gamepieces/Village.java
@@ -4,10 +4,14 @@ public class Village extends GamePiece {
 
 	private int value = 1;
 	
-	public int getValue() {
-		
+	public Village() {
+		super();
+		sprites = new String[]{"snow/beacon0.png","snow/beacon1.png","snow/beacon2.png","snow/beacon1.png"};
 		frame = 0;
 		frameSpeed = 35;
+	}
+	
+	public int getValue() {				
 		return value;
 	}
 }
