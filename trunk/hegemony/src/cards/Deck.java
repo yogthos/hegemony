48e5a822b3c0fa0044d0f23b774745193b344563
dmitri.sotnikov <dmitri.sotnikov@97c0824a-6a7c-11de-873c-fb8c264d332a>
2009-08-01 22:21:23 +0000

    added rules not to show highlights in setup phase

--- trunk/hegemony/src/cards/Deck.java
+++ trunk/hegemony/src/cards/Deck.java
@@ -1,10 +1,26 @@
 package cards;
 
+import game.BoardController;
+import game.ControlsPanel;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class Deck extends Stack<Card>{
+
+	private static final long serialVersionUID = 1L;
 	
-	public Deck() {
-		//TODO: add cards
+	List<Card> cards = new ArrayList<Card>();
+	
+	public Deck(ControlsPanel controls) {
+		generateDeck(controls);
+	}
+	
+	private void generateDeck(ControlsPanel controls) {
+		for (BoardController.MODE mode : BoardController.MODE.values()) {
+			cards.add(new Card(controls, mode));
+		}
+		
 	}
 }
