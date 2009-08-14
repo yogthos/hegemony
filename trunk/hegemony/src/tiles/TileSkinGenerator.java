20485783e5ca13032e0efa2ef3843d4ce8a2f108
dmitri.sotnikov <dmitri.sotnikov@97c0824a-6a7c-11de-873c-fb8c264d332a>
2009-08-14 04:52:36 +0000

    updated tiles

---
+++ trunk/hegemony/src/tiles/TileSkinGenerator.java
@@ -0,0 +1,27 @@
+package tiles;
+
+public class TileSkinGenerator {
+
+	private enum Type {
+		GRASS,
+		STONE,
+		TL,
+		TP,
+		TR,
+		BL,
+		BR,
+		BT
+	}
+	
+	private int xsize;
+	private int ysize;
+	
+	public TileSkinGenerator(int xsize, int ysize) {
+		this.xsize = xsize;
+		this.ysize = ysize;
+	}
+	
+	private void makeArtifact() {
+		
+	}
+}
